e92eef2ac1e78d7b4f706dddb9a884e04d4af100
Thais Andreatta <89612369+thaisand@users.noreply.github.com>
2022-03-25 00:37:36 -0300

    Rename Classee.java to E1/Classee.java

--- E1/Classee.java
+++ E1/Classee.java
@@ -4,9 +4,9 @@ class Classee {
 	public static Scanner sc = new Scanner(System.in);
 	public static void main (String args[]) {
 		int num1, num2, soma; 
-		System.out.println("Digite um número: ");
+		System.out.println("Digite um nÃºmero: ");
 		num1 = sc.nextInt();
-		System.out.println("Digite outro número: ");
+		System.out.println("Digite outro nÃºmero: ");
 		num2 = sc.nextInt();
 		soma = num1 + num2;
 		System.out.println("Soma = " + soma);

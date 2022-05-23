57f715811d4bbcc7d88674666a1b778d1f0d3b7b
Thais Andreatta <89612369+thaisand@users.noreply.github.com>
2022-05-23 15:39:29 -0300

    Create tradutor.java

---
+++ E4/tradutor.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import com.google.gson.*;
+import com.squareup.okhttp.*;
+
+public class Translate {
+    private static String subscriptionKey = "YOUR_SUBSCRIPTION_KEY";
+
+    // Add your location, also known as region. The default is global.
+    // This is required if using a Cognitive Services resource.
+    private static String location = "YOUR_RESOURCE_LOCATION";
+
+    HttpUrl url = new HttpUrl.Builder()
+        .scheme("https")
+        .host("api.cognitive.microsofttranslator.com")
+        .addPathSegment("/translate")
+        .addQueryParameter("api-version" "3.0")
+        .addQueryParameter("from" "en")
+        .addQueryParameter("to" "de")
+        .addQueryParameter("to" "it")
+        .build();
+
+    // Instantiates the OkHttpClient.
+    OkHttpClient client = new OkHttpClient();
+
+    // This function performs a POST request.
+    public String Post() throws IOException {
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType
+                "[{"Text": "Hello World!"}]");
+        Request request = new Request.Builder().url(url).post(body)
+                .addHeader("Ocp-Apim-Subscription-Key", subscriptionKey)
+                .addHeader("Ocp-Apim-Subscription-Region", location)
+                .addHeader("Content-type" "application/json")
+                .build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+
+    // This function prettifies the json response.
+    public static String prettify(String json_text) {
+        JsonParser parser = new JsonParser();
+        JsonElement json = parser.parse(json_text);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        return gson.toJson(json);
+    }
+
+    public static void main(String[] args) {
+        try {
+            Translate translateRequest = new Translate();
+            String response = translateRequest.Post();
+            System.out.println(prettify(response));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
